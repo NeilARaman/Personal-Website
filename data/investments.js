0235d41fc4096269766191ff8cbafeb3804aa356
Neil Raman <neilr@andrew.cmu.edu>
2025-08-13 08:53:19 -0500

    Remove 'Telora' investment entry from investments.js to streamline the investment list.

--- data/investments.js
+++ data/investments.js
@@ -1,13 +1,4 @@
 const investments = [
-  {
-    title: 'Telora',
-    description: 'Helping hackers start startups.',
-    icon: 'projects',
-    url: 'https://telora.com',
-    stage: 'Seed',
-    year: '2025',
-    featured: true,
-  },
   // Future investments will be added here
 ]
 
